dc9098e97c033555e28bec39a1a837d4e0694fec
paolabris <pamodiazbr@ittepic.edu.mx>
2022-09-11 21:40:37 -0600

    cambiar logo y nuevo main

---
+++ app/src/main/java/mx/edu/ittepic/ladm_u1_practica1_diseomenuprincipal/MainActivity2.kt
@@ -0,0 +1,11 @@
+package mx.edu.ittepic.ladm_u1_practica1_diseomenuprincipal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
