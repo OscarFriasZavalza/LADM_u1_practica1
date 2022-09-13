437c061d0a65adffb9a45a66d33a835ceabd0b79
paolabris <pamodiazbr@ittepic.edu.mx>
2022-09-12 20:27:20 -0600

    3 y 5 terminado

--- app/src/main/java/mx/edu/ittepic/ladm_u1_practica1_diseomenuprincipal/MainActivity3.kt
+++ app/src/main/java/mx/edu/ittepic/ladm_u1_practica1_diseomenuprincipal/MainActivity3.kt
@@ -1,11 +1,63 @@
 package mx.edu.ittepic.ladm_u1_practica1_diseomenuprincipal
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import mx.edu.ittepic.ladm_u1_practica1_diseomenuprincipal.databinding.ActivityMain2Binding
+import mx.edu.ittepic.ladm_u1_practica1_diseomenuprincipal.databinding.ActivityMain3Binding
+import java.io.OutputStreamWriter
 
 class MainActivity3 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        lateinit var binding: ActivityMain3Binding
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main3)
+        binding=ActivityMain3Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.pizza.setOnClickListener{
+            guardarenarchivo("Pizza Total a pagar $195")
+        }
+        binding.hamburguesa.setOnClickListener{
+            guardarenarchivo("Hamburguesa Total a pagar $90")
+        }
+        binding.camarones.setOnClickListener{
+            guardarenarchivo("Camarones al gusto Total a pagar $210")
+        }
+        binding.aguachile.setOnClickListener{
+            guardarenarchivo("Aguachile Total a pagar $120")
+        }
+        binding.carne.setOnClickListener{
+            guardarenarchivo("Rib Eye Total a pagar $250")
+        }
+        binding.pasta.setOnClickListener{
+            guardarenarchivo("Pasta Total a pagar $115")
+        }
+
+        binding.pozole.setOnClickListener{
+            guardarenarchivo("Pozole Total a pagar $120")
+        }
+        binding.enchiladas.setOnClickListener{
+            guardarenarchivo("Enchiladas Total a pagar $100")
+        }
+
+        binding.ordenar.setOnClickListener {
+            var activityorden= Intent(this,MainActivity7::class.java)
+            startActivity(activityorden)
+        }
+
+    }
+
+    fun guardarenarchivo(mensaje:String){
+        try {
+            var archivo= OutputStreamWriter(openFileOutput("datos.txt", MODE_PRIVATE))
+            archivo.write(mensaje)
+            archivo.flush()
+            archivo.close()
+            Toast.makeText(this,"Se guardo tu compra, ve al carrito", Toast.LENGTH_LONG).show()
+        }catch (e:Exception){
+            Toast.makeText(this,e.message, Toast.LENGTH_LONG).show()
+        }
+
     }
 }
--- app/src/main/java/mx/edu/ittepic/ladm_u1_practica1_diseomenuprincipal/MainActivity5.kt
+++ app/src/main/java/mx/edu/ittepic/ladm_u1_practica1_diseomenuprincipal/MainActivity5.kt
@@ -1,11 +1,62 @@
 package mx.edu.ittepic.ladm_u1_practica1_diseomenuprincipal
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import mx.edu.ittepic.ladm_u1_practica1_diseomenuprincipal.databinding.ActivityMain2Binding
+import mx.edu.ittepic.ladm_u1_practica1_diseomenuprincipal.databinding.ActivityMain5Binding
+import java.io.OutputStreamWriter
 
 class MainActivity5 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        lateinit var binding: ActivityMain5Binding
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main5)
+        binding=ActivityMain5Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.whisky.setOnClickListener{
+            guardarenarchivo("Whisky Total a pagar $60")
+        }
+        binding.vodka.setOnClickListener{
+            guardarenarchivo("Vodka Total a pagar $120")
+        }
+        binding.clericot.setOnClickListener{
+            guardarenarchivo("Clericot Total a pagar $60")
+        }
+        binding.vino.setOnClickListener{
+            guardarenarchivo("Vino tinto Total a pagar $200")
+        }
+        binding.mojito.setOnClickListener{
+            guardarenarchivo("Mojito Total a pagar $50")
+        }
+        binding.limonada.setOnClickListener{
+            guardarenarchivo("Limonada Total a pagar $30")
+        }
+
+        binding.cerveza.setOnClickListener{
+            guardarenarchivo("Cerveza Total a pagar $30")
+        }
+        binding.refresco.setOnClickListener{
+            guardarenarchivo("Refresco Total a pagar $20")
+        }
+
+        binding.orden2.setOnClickListener {
+            var activityorden2= Intent(this,MainActivity7::class.java)
+            startActivity(activityorden2)
+        }
+    }
+
+    fun guardarenarchivo(mensaje:String){
+        try {
+            var archivo= OutputStreamWriter(openFileOutput("datos.txt", MODE_PRIVATE))
+            archivo.write(mensaje)
+            archivo.flush()
+            archivo.close()
+            Toast.makeText(this,"Se guardo tu compra, ve al carrito", Toast.LENGTH_LONG).show()
+        }catch (e:Exception){
+            Toast.makeText(this,e.message, Toast.LENGTH_LONG).show()
+        }
+
     }
 }
